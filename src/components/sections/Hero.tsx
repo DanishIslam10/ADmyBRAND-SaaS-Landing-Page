afa1d11fa7a64918862652cabf790a65d781b479
DanishIslam <danishislam328@gmail.com>
2025-07-29 00:38:37 +0530

    Link component

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,6 +2,7 @@
 
 import { motion } from 'framer-motion'
 import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Hero() {
   return (
@@ -11,7 +12,7 @@ export default function Hero() {
 
       {/* Content */}
       <div className="relative z-10 flex flex-col md:flex-row items-center justify-between gap-12 md:gap-8 md:py-16 w-full max-w-7xl">
-        
+
         {/* Text Content */}
         <div className="w-full md:w-1/2 text-center md:text-left px-2">
           <motion.h1
@@ -42,24 +43,24 @@ export default function Hero() {
             className="flex sm:flex-row gap-4 mt-6 justify-center md:justify-start"
           >
             {/* Start Free Trial Button */}
-            <a href="#_" className="w-fit relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+            <Link href="#_" className="w-fit relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
               <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] absolute transition-all duration-500 ease-out group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05]" />
               <span className="relative px-6 py-3 bg-gray-900 bg-gradient-to-br hover:from-[#ff8a05] hover:via-[#ff5478] hover:to-[#ff00c6] rounded-md transition-all duration-500 ease-out group-hover:bg-opacity-0">
                 <span className="relative text-white">Start Free Trial</span>
               </span>
-            </a>
+            </Link>
 
             {/* Book a Demo Button */}
-            <a href="#_" className="w-fit relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group">
-              <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100" />
-              <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3" />
-              <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5" />
-              <span className="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5" />
-              <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5" />
-              <span className="absolute inset-0 w-full h-full border border-white rounded-md opacity-10" />
-              <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5" />
-              <span className="relative">Book A Demo</span>
-            </a>
+            <Link href="#_" className="w-fit relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group">
+                <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100" />
+                <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3" />
+                <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5" />
+                <span className="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5" />
+                <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5" />
+                <span className="absolute inset-0 w-full h-full border border-white rounded-md opacity-10" />
+                <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5" />
+                <span className="relative">Book A Demo</span>
+            </Link>
           </motion.div>
         </div>
 
